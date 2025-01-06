35bd1d16686f5c2accc84bac76f1f3d9f4b62423
Ravi-Theja-pallikonda <ravitheja.pallikonda@gmail.com>
2025-01-06 18:05:24 +0100

    added multiplication

--- src/addition.py
+++ src/addition.py
@@ -13,3 +13,10 @@ def sub(a, b):
 def test_sub():
     assert sub(2, 1) == 1
     assert sub(1, -1) == 2
+
+def mul(a, b):
+    return a * b
+
+def test_mul():
+    assert mul(2, 1) == 2
+    assert mul(1, -1) == -1
